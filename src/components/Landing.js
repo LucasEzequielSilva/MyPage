2b99720d3b31481358c953016558c75baec00de0
mrain00 <luxassilva@gmail.com>
2022-04-28 19:55:37 -0300

    okey

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Navbar from './Navbar'
 import '../styles/landing.css'
 import yo from '../assets/yo.png'
+import { ParallaxProvider, useParallax } from 'react-scroll-parallax';
+/* SVGS */
+import Cloud from '@mui/icons-material/CloudQueueRounded';
+import Code from '@mui/icons-material/CodeRounded';
+import Color from '@mui/icons-material/ColorLensRounded';
+/* ANIMACION SLIDE */
+import Slide from 'react-reveal/Slide';
 const Landing = () => {
+    const parallax = useParallax({
+        speed: 30,
+    });
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+  
     return (
-        <div className="bg-gray-50 h-screen flex flex-col items-center w-full landing-container">
-            <Navbar className="w-full top-0" />
-            <div className=" w-full flex items-center justify-between container-home">
+
+        <>
+            <div className="bg-gray-50 h-screen flex flex-col items-center w-full landing-container">
+                <Navbar className="w-full top-0" />
+                <img className='sticker' src='https://user-images.githubusercontent.com/91817152/165396732-662d0d38-9c9c-43be-8459-3fe70d3a7a65.png' alt='stickerhttps://user-images.githubusercontent.com/91817152/165396732-662d0d38-9c9c-43be-8459-3fe70d3a7a65.png' />
                 <section>
-                    <h1 className="call-to-action">
-                        Hola, soy Lucas Silva
-                        <img className='emoji-feliz' src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/325/grinning-face_1f600.png" />
-                    </h1>
+                    <Slide left>
+                        <h1 className="call-to-action">
+                            Hola, soy Lucas Silva
+                            <img className='emoji-feliz' src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/325/grinning-face_1f600.png" />
+                        </h1>
                     <p className='parraph'>
-                        Desarrollador Full Stack MERN (MongoDB, Express, ReactJS, NodeJS) con preferencia al Frontend.
+                        Desarrollador Full Stack MERN (MongoDB, Express, ReactJS, NodeJS). Si quieres conocerme solo dale click!
                     </p>
                     <button className='button'>
                         <span className="text">Empecemos!</span>
@@ -22,16 +39,86 @@ const Landing = () => {
                         <span className="blob"></span>
                         <span className="blob"></span>
                     </button>
+                </Slide>
                 </section>
-                <div className="yo-container flex items-center justify-center">
+
+                <Slide left>
+
+                <div className="container-yo">
+
                     <img src={yo} alt='yo' className='yo z-10' />
-                    <div className="circle absolute bottom-32 w-72 h-72 mix-blend-multiply bg-purple-300 rounded-full mix-blend filter blur-xl opacity-70 animate-blob animation-delay-2000 delay-200"></div>
-                    <div className="circle absolute left-32 w-72 h-72 mix-blend-multiply bg-yellow-300 rounded-full mix-blend filter blur-xl opacity-70"></div>
-                    <div className="circle absolute right-40 w-72 h-72 mix-blend-multiply bg-pink-300 rounded-full mix-blend filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
                 </div>
-                <div className="circle circle-bottom absolute bg-sky-300 rounded-full mix-blend filter blur-3xl opacity-40 animate-blob animation-delay-4000"></div>
+                </Slide>
+
+                <div className="circle-violet absolute w-72 h-72 bg-sky-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+                <div className="circle-yellow absolute bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-90 animate-blob animation-delay-2000"></div>
+                <div className="circle-rose absolute bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
             </div>
-        </div>
+            <div className='services'>
+                <div className='element-1' ref={parallax.ref} >
+                    <img src='https://user-images.githubusercontent.com/91817152/165396694-27463b8f-571b-4fcb-9537-fbe7c434fe0e.svg' />
+                </div>
+                <div className='container-title'>
+                    <h1 className='service-title'>
+                        Sobre mi<span className='service-punto'>.</span>
+                    </h1>
+                    <img className='wave' src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/325/water-wave_1f30a.png' />
+                </div>
+                <div className='main-service'>
+                    <div className='habilities'>
+                        <span className='habilities-text'>
+                            MIS APTITUDES TÉCNICAS
+                        </span>
+                        <Slide left>
+
+                        <div className='frontend'>
+                            <img src='https://c1.wallpaperflare.com/preview/183/501/299/code-coding-programming-css.jpg' />
+                            <span className='frontend-text'>
+                                FRONTEND
+                            </span>
+                            <p className='parraph-cards'>
+                                El frontend es la parte del desarrollo web que se dedica a la parte frontal de un sitio web, en pocas palabras del diseño de un sitio web, desde la estructura del sitio hasta los estilos como colores, fondos, tamaños hasta llegar a las animaciones y efectos.
+                            </p>
+                            <Code className='svg'/>
+                        </div>
+                </Slide>
+
+                        <Slide left>
+
+                        <div className='backend'>
+                            <img src='https://www.bigdata4innovation.it/wp-content/uploads/sites/5/2020/03/shutterstock_403154125.jpg' />
+                            <span className='backend-text'>
+                                BACKEND
+                            </span>
+                            <p className='parraph-cards'>
+                                El Backend, también conocido como CMS o Backoffice, es la parte de la app que el usuario final no puede ver. Su función es acceder a la información que se solicita, a través de la app, para luego combinarla y devolverla al usuario final
+                            </p>
+                            <Cloud className='svg'/>
+                        </div>
+                </Slide>
+                <Slide left>
+
+                        <div className='design'>
+                            <img src='https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+                            <span className='design-text'>
+                                WEB DESIGN
+                            </span>
+                            <p className='parraph-cards'>
+                                El diseño web es un área enfocada en el desarrollo de interfaces digitales, como el diseño de sitios y aplicaciones para web. Para ello, los diseñadores web crean las páginas utilizando lenguajes de marcado como HTML.
+                            </p>
+                            <Color className='svg'/>
+                        </div>
+                </Slide>
+
+                    </div>
+                    <div className='me'>
+                        <span className='habilities-text'>
+                            HABILIDADES BLANDAS
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,160 +1,140 @@
-import React, { useState } from "react";
-import { Transition } from "@headlessui/react";
-import "../styles/navbar.css";
-import Logo from '../assets/MyLogo.png';
-function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import '../styles/navbar.css';
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div className="w-full top-0 navbar">
-                <div className="circle circle-nav absolute top-0 left-0 bg-purple-300 mix-blend filter blur-3xl animate-blob animation-delay-2000"></div>
-      <nav className="bg-transparent top-0">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0 border-gradient border-gradient-purple">
-                <h2 className="logo">
-                  Lucas
-                </h2>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <a
-                    href="#"
-                    className="nav-items text-white px-3 py-2 rounded-md text-sm font-medium transition ease-in-out delay-150 bg-fuchsia-300 hover:-translate-y-1 hover:scale-110 backdrop-blur-xl duration-300 "
-                  >
-                    Dashboard
-                  </a>
+    <AppBar position="static" className='navbar' sx={{boxShadow:"none", background:"none"}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            LUCAS
+          </Typography>
 
-                  <a
-                    href="#"
-                    className="nav-items text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Team
-                  </a>
-
-                  <a
-                    href="#"
-                    className="nav-items text-gray-300  hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Projects
-                  </a>
-
-                  <a
-                    href="#"
-                    className="nav-items text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Calendar
-                  </a>
-
-                  <a
-                    href="#"
-                    className="nav-items text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Reports
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem className='nav-items' sx={{color:'#000'}} key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center" sx={{color:'#000'}}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
               >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
+                {page}
+              </Button>
+            ))}
+          </Box>
 
-        <Transition
-          show={isOpen}
-          enter="transition ease-out duration-100 transform"
-          enterFrom="opacity-0 scale-95"
-          enterTo="opacity-100 scale-100"
-          leave="transition ease-in duration-75 transform"
-          leaveFrom="opacity-100 scale-100"
-          leaveTo="opacity-0 scale-95"
-        >
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="#"
-                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Dashboard
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Team
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Projects
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Reports
-                </a>
-              </div>
-            </div>
-          )}
-        </Transition>
-      </nav>
-    </div>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-}
-
-export default Navbar;
+};
+export default ResponsiveAppBar;
